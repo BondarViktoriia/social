91bf7891cd7847079ad5acbdd3ce3540c2a02f3c
BondarViktoriia <103380251+BondarViktoriia@users.noreply.github.com>
2023-03-21 14:23:01 +0200

    create main tab navigation + main screen

--- App.js
+++ App.js
@@ -2,16 +2,51 @@ import LoginScreen from "./Screens/auth/LoginScreen";
 import RegistrationScreen from "./Screens/auth/RegistrationScreen";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import PostScreen from "./Screens/main/PostsScreen";
+import CreatePostsScreen from "./Screens/main/CreatePostsScreen";
+import ProfileScreen from "./Screens/main/ProfileScreen";
 
 const AuthStack = createStackNavigator();
+const MainTab = createBottomTabNavigator();
+
+const useRoute = (isAuth) => {
+  if (!isAuth) {
+    return (<AuthStack.Navigator>
+      <AuthStack.Screen
+        options={{ headerShown: false }}
+        name="Login"
+        component={LoginScreen}
+      />
+      <AuthStack.Screen
+        options={{ headerShown: false }}
+        name="Registration"
+        component={RegistrationScreen}
+      />
+    </AuthStack.Navigator>);
+  }
+  return (
+    <MainTab.Navigator>
+      <MainTab.Screen
+        options={{ headerShown: false }}
+        name="Posts"
+        component={PostScreen}
+      />
+      <MainTab.Screen
+        options={{ headerShown: false }}
+        name="CreatePost"
+        component={CreatePostsScreen}
+      />
+      <MainTab.Screen
+        options={{ headerShown: false }}
+        name="Profile"
+        component={ProfileScreen}
+      />
+    </MainTab.Navigator>
+  );
+};
 
 export default function App() {
-  return (
-    <NavigationContainer>
-      <AuthStack.Navigator>
-        <AuthStack.Screen  options={{headerShown:false}} name="Login" component={LoginScreen} />
-        <AuthStack.Screen options={{headerShown:false}} name="Registration" component={RegistrationScreen} />
-      </AuthStack.Navigator>
-    </NavigationContainer>
-  );
+  const routing = useRoute(true);
+  return <NavigationContainer>{routing}</NavigationContainer>;
 }
